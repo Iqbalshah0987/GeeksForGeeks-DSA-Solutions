3b53f997f0dc2edf1e8570fff7daaf03a6bf97dd
Iqbal Shah <98959482+Iqbalshah0987@users.noreply.github.com>
2026-09-13 22:48:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
@@ -0,0 +1,33 @@
+/* Structure of a Binary Search Tree node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* findLCA(Node* root, Node* n1, Node* n2) {
+        // code here
+        if(root == NULL){
+            return NULL;
+        }
+        
+        if(root->data < n1->data && root->data < n2->data){
+            return findLCA(root->right, n1, n2);
+        }
+        if(root->data > n1->data && root->data > n2->data){
+            return findLCA(root->left, n1, n2);
+        }
+        
+        return root;
+    }
+};
