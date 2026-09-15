3f95654516dd46138c90dd7c4894e1febcc84cb5
Iqbal Shah <98959482+Iqbalshah0987@users.noreply.github.com>
2026-09-15 20:53:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Heap implementation/max-heap-implementation.cpp
+++ Difficulty: Easy/Max Heap implementation/max-heap-implementation.cpp
@@ -5,8 +5,8 @@ class maxHeap {
     
     void heapify(vector<int> &arr, int n, int i){
         int largest = i;
-        int left = 2*i;
-        int right = 2*i+1;
+        int left = 2*i+1;
+        int right = 2*i+2;
         
         if(left<n && arr[largest]<arr[left]){
             largest = left;
